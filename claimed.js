c7101e55c6212060b2a896b2798a06517d3ae7aa
oxmikem-site <threemmmbooks@gmail.com>
2025-06-16 17:27:41 +0100

    Add files via upload

---
+++ claimed.js
@@ -0,0 +1,5 @@
+// Addresses that already secured a GTD free spot
+const claimed = [
+  "bc1pp0s5et5kmdhu23gtkfem5ag5wf30vvs3ytzp6sajnp28gpvqmggq5a7tqw"
+  // Add new addresses here manually after someone secures
+];
