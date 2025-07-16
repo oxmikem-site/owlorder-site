a4bb70278a80b51313dca8ed505632557adafd82
oxmikem-site <threemmmbooks@gmail.com>
2025-07-16 12:32:31 +0100

    Update index.umd.js

--- libs/sats-connect/index.umd.js
+++ libs/sats-connect/index.umd.js
@@ -1,23 +1,33 @@
+(function(global) {
+  "use strict";
+  console.log("Standalone sats-connect UMD loaded");
 
-(function(root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define([], factory);
-  } else if (typeof exports === 'object') {
-    module.exports = factory();
-  } else {
-    root.satsConnect = factory();
-  }
-}(this, function() {
-  console.log("sats-connect UMD loaded");
-
-  async function getAddress(opts) {
-    return {
-      address: "bc1qexampleaddress1234567890",
-      publicKey: "examplepublickey=="
-    };
+  async function selectWallet({ provider, network }) {
+    alert("Simulated selectWallet: " + provider + " / " + network.type);
+    return { provider, network };
   }
 
-  return {
-    getAddress: getAddress
+  async function getAccounts() {
+    return [
+      {
+        address: "bc1qexampleaddress1234567890",
+        publicKey: "03examplepublickeyabcdef",
+      },
+    ];
+  }
+
+  async function signMessage() {
+    alert("Simulated signMessage");
+  }
+
+  async function sendBtcTransaction() {
+    alert("Simulated sendBtcTransaction");
+  }
+
+  global.satsConnect = {
+    selectWallet,
+    getAccounts,
+    signMessage,
+    sendBtcTransaction
   };
-}));
+})(typeof window !== "undefined" ? window : this);
