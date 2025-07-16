4a698a43703c65ce8b24ebeac1d82f412d32df97
oxmikem-site <threemmmbooks@gmail.com>
2025-07-16 12:07:06 +0100

    Update index.umd.js

--- libs/sats-connect/index.umd.js
+++ libs/sats-connect/index.umd.js
@@ -1,1 +1,2 @@
+(function(e,n){typeof exports=="object"&&typeof module<"u"?n(require("@sats-connect/core")):typeof define=="function"&&define.amd?define(["@sats-connect/core"],n):(e=typeof globalThis<"u"?globalThis:e||self,n(e.SatsConnectCore))})(this,function(e){"use strict";console.log("sats-connect core UMD loaded"),window.satsConnect={showConnectModal:e.showConnectModal,getAddress:e.getAddress,sendBtcTransaction:e.sendBtcTransaction,signMessage:e.signMessage}});
 
